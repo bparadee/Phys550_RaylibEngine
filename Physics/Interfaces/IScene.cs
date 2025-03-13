deebe36d0d044e923c4bac26435c70118f7b70c9
Brian Paradee <bjparadee@gmail.com>
2025-03-13 15:15:32 -0700

    Updated member variables

---
+++ Physics/Interfaces/IScene.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Physics550Engine_Raylib.Physics.Interfaces
+{
+    public interface IScene
+    {
+        public void Step();
+
+    }
+}
